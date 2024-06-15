bc87a3173146705f77873c5ca46dc8e18453c951
Vitaly Gashkov <vitalygashkov@vk.com>
2024-06-15 13:24:46 +0500

    feat: log key id with state

--- eme-logger-mod.js
+++ eme-logger-mod.js
@@ -20,4 +20,29 @@
     if (pssh) window.postMessage({ type: '38405bbb-36ef-454d-8b32-346f9564c978', log: pssh }, '*');
     return _target.apply(_this, _args);
   });
+
+  function arrayBufferToHex(arrayBuffer) {
+    if (typeof arrayBuffer !== 'object' || arrayBuffer === null || typeof arrayBuffer.byteLength !== 'number') {
+      throw new TypeError('Expected input to be an ArrayBuffer');
+    }
+    var view = new Uint8Array(arrayBuffer);
+    var result = '';
+    var value;
+    for (var i = 0; i < view.length; i++) {
+      value = view[i].toString(16);
+      result += value.length === 1 ? '0' + value : value;
+    }
+    return result;
+  }
+
+  proxy(MediaKeySession.prototype, 'update', async (_target, _this, _args) => {
+    setTimeout(() => {
+      const statuses = Array.from(_this.keyStatuses);
+      for (const [kidBuffer, state] of statuses) {
+        const kid = arrayBufferToHex(kidBuffer);
+        console.log(`KID:STATE -> ${kid}:${state}`);
+      }
+    }, 3000);
+    return _target.apply(_this, _args);
+  });
 })();
