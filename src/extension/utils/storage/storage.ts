eb00e55a66a275e61db99b624ccfab98a434ad9d
Vitaly Gashkov <vitalygashkov@vk.com>
2024-10-29 21:56:43 +0500

    feat(extension): add icons

--- src/extension/utils/storage/storage.ts
+++ src/extension/utils/storage/storage.ts
@@ -1,4 +1,4 @@
-import { storage, WatchCallback } from 'wxt/storage';
+import { storage } from 'wxt/storage';
 import { Client, fromBase64, fromBuffer } from '../../../lib';
 import { asJson } from './utils';
 
